74c41b90dd9874a13464c610820d806ecb94d368
Jeff Triplett <jeff.triplett@gmail.com>
2017-07-28 22:46:25 -0500

    :art: Adds player face as an app icon

--- conf.lua
+++ conf.lua
@@ -8,7 +8,7 @@ function love.conf(t)
     t.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean)
 
     t.window.title = "Ludum Dare 39"    -- The window title (string)
-    t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
+    t.window.icon = "assets/playerFace_dark.png"  -- Filepath to an image to use as the window's icon (string)
     t.window.width = 800                -- The window width (number)
     t.window.height = 600               -- The window height (number)
     t.window.borderless = false         -- Remove all border visuals from the window (boolean)
