b7abea36d3bae1889030e8ced4cd63164caf95a1
Jeff Triplett <jeff.triplett@gmail.com>
2017-07-28 21:46:43 -0500

    :sparkles: Adds a moveable green dot

--- main.lua
+++ main.lua
@@ -7,6 +7,13 @@ local Gamestate = require 'lib/vendor/hump/gamestate'
 
 local Title = {}
 local Game = {}
+local Player = {
+    x = 0,
+    y = 0,
+    width = 16,
+    height = 16,
+    speed = 64,
+}
 
 
 -- Love section
@@ -35,13 +42,43 @@ end
 -- Main game section
 
 function Game:draw()
+    love.graphics.setColor(128, 255, 128)
+    love.graphics.rectangle(
+        'fill',
+        Player.x,
+        Player.y,
+        Player.width,
+        Player.height
+    )
+
+    love.graphics.setColor(255, 255, 255)
     love.graphics.print('Game:draw()', 16, 16)
+
     fps = love.timer.getFPS()
     love.graphics.print(
         'fps: '..('%3d'):format(fps),
         love.graphics.getWidth() - 64,
         16
     )
+
+    love.graphics.print(
+        'Player: (' .. ('%d'):format(Player.x) .. ', ' .. ('%d'):format(Player.y) .. ')',
+        16,
+        32
+    )
+end
+
+
+function Game:update(deltatime)
+    if love.keyboard.isDown('up') or love.keyboard.isDown('w') then
+        Player.y = Player.y - Player.speed * deltatime
+    elseif love.keyboard.isDown('down') or love.keyboard.isDown('s') then
+        Player.y = Player.y + Player.speed * deltatime
+    elseif love.keyboard.isDown('left') or love.keyboard.isDown('a') then
+        Player.x = Player.x - Player.speed * deltatime
+    elseif love.keyboard.isDown('right') or love.keyboard.isDown('d') then
+        Player.x = Player.x + Player.speed * deltatime
+    end
 end
 
 
