1a4348120a2d94a825a010c22c80ccd534c0c297
Jeff Triplett <jeff.triplett@gmail.com>
2017-07-28 21:09:03 -0500

    :sparkles: Adds title and main game states

--- main.lua
+++ main.lua
@@ -1,4 +1,52 @@
+-- Modules
 
-function love:draw()
-    love.graphics.print('Press Enter to continue', 16, 16)
+local Gamestate = require 'lib/vendor/hump/gamestate'
+
+
+-- Global variables
+
+local Title = {}
+local Game = {}
+
+
+-- Love section
+
+function love.load()
+    Gamestate.registerEvents()
+    Gamestate.switch(Title)
+end
+
+
+-- Title section
+
+function Title:draw()
+    love.graphics.print('Title:draw()', 16, 16)
+    love.graphics.print('Press Enter to continue', 16, 32)
+end
+
+
+function Title:keyreleased(key, code)
+    if key == 'return' then
+        Gamestate.switch(Game)
+    end
+end
+
+
+-- Main game section
+
+function Game:draw()
+    love.graphics.print('Game:draw()', 16, 16)
+end
+
+
+function Game:keypressed(key, code)
+    if love.keyboard.isDown('escape') or love.keyboard.isDown('q') then
+        love.event.quit()
+    end
+end
+
+
+function Game:quit()
+    print('Thank you for playing!')
+    return false
 end
