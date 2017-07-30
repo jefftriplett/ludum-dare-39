7cda68ba44009152f0c7249c03240837b110f886
Jeff Triplett <jeff.triplett@gmail.com>
2017-07-30 17:13:49 -0500

    :sparkles: Adds collision detection which works
    Signed-off-by: Jeff Triplett <jeff.triplett@gmail.com>

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 -- Modules
 
-local Anim8 = require('lib/vendor/anim8/anim8')
-local Gamestate = require('lib/vendor/hump/gamestate')
-local Sti = require('lib/vendor/sti')
-
+local Anim8 = require('lib.vendor.anim8.anim8')
+local Bump = require('lib.vendor.bump.bump')
+local Gamestate = require('lib.vendor.hump.gamestate')
+local Sti = require('lib.vendor.sti')
 
 -- Global variables
 
@@ -13,21 +13,37 @@ local TitleScreen = {}
 local Font = nil
 local Map = nil
 local World = nil
+local Debug = true  -- TODO: Change before release
 
 local Player = {
     x = nil,
     y = nil,
-    offset_x = math.floor(64 / 2),
-    offset_y = math.floor(64 / 1.35),
+    offset_x = 16,  -- I eyeballed the x and y to get "right"
+    offset_y = 32,
     rotation = 0,
-    width = 64,
-    height = 64,
+    width = 32,
+    height = 16,
     speed = 128,
+    collidable = true,
+    sensor = true,
     durations = 0.25,  -- or this can be {0.5, 0.1, ...} based on each frame
     animation = nil,  -- our index for holding which Quad we are displaying
     animations = nil,  -- our Quads
     spritesheet = nil,  -- our image spritesheet which holds all of our images
 }
+
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
 
 
 -- Love section
@@ -86,12 +102,12 @@ function love.load()
 
     love.physics.setMeter(32)
 
-    Map = Sti('assets/levels/level0001.lua', {'box2d'})
+    Map = Sti('assets/levels/level0001.lua', {'bump'})
 
-    World = love.physics.newWorld(0, 0)
+    World = Bump.newWorld(64)
 
     -- Prepare collision objects
-    Map:box2d_init(World)
+    Map:bump_init(World)
 
     Map:addCustomLayer('Sprite Layer', 4)
 
@@ -102,6 +118,7 @@ function love.load()
             spawn_point = object
             Player.x = spawn_point.x
             Player.y = spawn_point.y
+            World:add(Player, Player.x, Player.y, Player.width, Player.height)
             break
         end
     end
@@ -136,11 +153,15 @@ function love.load()
 
             -- Temporarily draw a point at our location so we know
             -- that our sprite is offset properly
-            love.graphics.setPointSize(5)
-            love.graphics.points(
-                math.floor(sprite.x),
-                math.floor(sprite.y)
-            )
+            if Debug then
+                love.graphics.rectangle(
+                    'fill',
+                    math.floor(sprite.x),
+                    math.floor(sprite.y),
+                    math.floor(sprite.width),
+                    math.floor(sprite.height)
+                )
+            end
         end
     end
 
@@ -154,6 +175,7 @@ end
 function GameScreen:draw()
     -- Draw hud
     love.graphics.setColor(255, 255, 255)
+    -- love.graphics.setColor(89, 106, 108)
     love.graphics.print('GameScreen:draw()', 16, 16)
 
     fps = love.timer.getFPS()
@@ -174,12 +196,17 @@ function GameScreen:draw()
     Map:draw(0, 64)
 
     -- Draw Collision Map (useful for debugging)
-    love.graphics.setColor(0, 0, 255)
-    Map:box2d_draw(0, 64)
+    if Debug then
+        love.graphics.setColor(0, 0, 255)
+        Map:bump_draw(World, 0, 64, 1, 1)
+    end
 end
 
 
 function GameScreen:update(deltatime)
+    -- Update our Map to keep everything in sync
+    Map:update(deltatime)
+
     -- We only want to animate the player animation when we are moving
     animate_player = false
 
@@ -211,8 +238,30 @@ function GameScreen:update(deltatime)
         Player.animation:update(deltatime)
     end
 
-    -- Update our Map to keep everything in sync
-    Map:update(deltatime)
+    Player.x, Player.y, collisions, collision_len = World:move(Player, Player.x, Player.y)
+    if collision_len > 0 then
+        if Debug then
+            print(collision_len)
+            print(dump(collisions))
+        end
+    end
+
+    -- TODO: Do something with this...
+    for i,v in ipairs (collisions) do
+        if collisions[i].normal.y == -1 then
+            -- player.yvel = 0
+            -- player.grounded = true
+            -- debug = debug.."Collided "
+            if Debug then
+                print('Collided')
+            end
+        elseif collisions[i].normal.y == 1 then
+            -- player.yvel = -player.yvel/4
+        end
+        if collisions[i].normal.x ~= 0 then
+            -- player.xvel = 0
+        end
+    end
 end
 
 
