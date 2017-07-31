9b9575e4490720b6c3fb7d44e5d33a08734f3ea9
Jeff Triplett <jeff.triplett@gmail.com>
2017-07-30 20:38:27 -0500

    :tractor: Re-factors gamestates to external files
    Signed-off-by: Jeff Triplett <jeff.triplett@gmail.com>

---
+++ gamestates/title.lua
@@ -0,0 +1,30 @@
+
+-- TitleScreen handles our main title screen
+
+local Gamestate = require('lib.vendor.hump.gamestate')
+
+local Font = nil
+
+local TitleScreen = Gamestate.new()
+
+
+function TitleScreen:enter()
+    Font = love.graphics.newFont('assets/fonts/Kenney Pixel.ttf', 32)
+    love.graphics.setFont(Font)
+end
+
+
+function TitleScreen:draw()
+    love.graphics.print('TitleScreen:draw()', 16, 16)
+    love.graphics.print('Press Enter to continue', 16, 32)
+end
+
+
+function TitleScreen:keyreleased(key, code)
+    if key == 'return' then
+        Gamestate.switch(gamestates.game)
+    end
+end
+
+
+return TitleScreen
