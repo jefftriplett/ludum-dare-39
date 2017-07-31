3d999613b4d1a04288f3f32a5b40d849d668d7ca
Jeff Triplett <jeff.triplett@gmail.com>
2017-07-31 16:54:59 -0500

    :tractor: Adds entities and movable boxes
    Signed-off-by: Jeff Triplett <jeff.triplett@gmail.com>

--- gamestates/game.lua
+++ gamestates/game.lua
@@ -3,14 +3,15 @@
 
 local Anim8 = require('lib.vendor.anim8.anim8')
 local Bump = require('lib.vendor.bump.bump')
+local Class = require('lib.vendor.middleclass.middleclass')
 local Gamestate = require('lib.vendor.hump.gamestate')
 local Inspect = require('lib.vendor.inspect.inspect')
 local Sti = require('lib.vendor.sti')
 
-
 local GameScreen = Gamestate.new()
 
 local Font = nil
+local Grid = nil
 local Map = nil
 local World = nil
 local Debug = false  -- TODO: Change before release
@@ -61,6 +62,7 @@ local Levels = {
 local StartingLevel = 1
 local CurrentLevel = StartingLevel
 
+-- TODO: Turn our player into an Entity
 local Player = {
     x = nil,
     y = nil,
@@ -70,6 +72,7 @@ local Player = {
     width = 32,
     height = 16,
     speed = 128,
+    type = 'player',
     collidable = true,
     sensor = true,
     durations = 0.25,  -- or this can be {0.5, 0.1, ...} based on each frame
@@ -77,6 +80,58 @@ local Player = {
     animations = nil,  -- our Quads
     spritesheet = nil,  -- our image spritesheet which holds all of our images
 }
+
+local Targets = nil
+local Entities = nil
+
+local Entity = Class('Entity')
+
+function Entity:initialize()
+    self.entity_id = nil  -- this is how we tell from entity from another
+    self.x = nil
+    self.y = nil
+    self.offset_x = 0
+    self.offset_y = 0
+    self.rotation = 0
+    self.width = 64
+    self.height = 64
+    self.speed = 128
+    self.type = 'unknown'
+    self.collidable = true
+    self.sensor = true
+    self.durations = 0.25  -- or this can be {0.5, 0.1, ...} based on each frame
+    self.animation = nil  -- our index for holding which Quad we are displaying
+    self.animations = nil  -- our Quads
+    self.spritesheet = nil  -- our image spritesheet which holds all of our images
+    self.properties = {}
+    self.properties.movable = true
+
+    self.animations = {
+        default = Anim8.newAnimation(
+            Grid(
+                2, 1,
+                2, 2
+            ),
+            self.durations
+        ),
+        on_target = Anim8.newAnimation(
+            Grid(
+                2, 2
+            ),
+            self.durations
+        )
+    }
+    self.animation = self.animations.default
+    self.spritesheet = Player.spritesheet
+end
+
+
+local Box = Class('Box', Entity)
+
+function Box:initialize()
+    Entity.initialize(self)
+    self.type = 'box'
+end
 
 
 function GameScreen:draw()
@@ -117,11 +172,11 @@ function GameScreen:enter()
     love.graphics.setFont(Font)
 
     Player.spritesheet = love.graphics.newImage('assets/images/sokoban_tilesheet.png')
-    local grid = Anim8.newGrid(64, 64, Player.spritesheet:getWidth(), Player.spritesheet:getHeight())
+    Grid = Anim8.newGrid(64, 64, Player.spritesheet:getWidth(), Player.spritesheet:getHeight())
 
     Player.animations = {
         down = Anim8.newAnimation(
-            grid(
+            Grid(
                 1, 6,
                 2, 6,
                 1, 6,
@@ -130,7 +185,7 @@ function GameScreen:enter()
             Player.durations
         ),
         up = Anim8.newAnimation(
-            grid(
+            Grid(
                 4, 6,
                 5, 6,
                 4, 6,
@@ -139,7 +194,7 @@ function GameScreen:enter()
             Player.durations
         ),
         right = Anim8.newAnimation(
-            grid(
+            Grid(
                 1, 8,
                 2, 8,
                 1, 8,
@@ -148,7 +203,7 @@ function GameScreen:enter()
             Player.durations
         ),
         left = Anim8.newAnimation(
-            grid(
+            Grid(
                 4, 8,
                 5, 8,
                 4, 8,
@@ -164,6 +219,42 @@ function GameScreen:enter()
     love.physics.setMeter(32)
 
     GameScreen:load_level(Levels[CurrentLevel].filename)
+
+    local sprite_layer = Map.layers['Sprite Layer']
+    local target_group = Map.layers['targets']
+
+    -- TODO: We need to refactor / move this target finding code
+
+    Targets = {}
+
+    print('v================================================================')
+    print(Inspect(target_group))
+    print('v----------------------------------------------------------------')
+    print('height: ' .. target_group.height)
+    print('width:  ' .. target_group.width)
+    for y, target in ipairs(target_group.data) do
+        if next(target) ~= nil then
+            print('index: ' .. y)
+            print(Inspect(target))
+
+            for x, tile in pairs(target) do
+                tile_index = x + y * target_group.width + 1
+                print('x: ' .. x)
+                print('y: ' .. y)
+                print('width:' .. target_group.width)
+                print('tile index:' .. tile_index)
+                print('is_target:', tile.properties.target)
+                print(tile.gid)
+                -- tile.gid = 27
+                Targets[tile_index] = tile
+                -- target_group.data[tile_index] = 27
+            end
+        end
+    end
+
+    print('Targets:')
+    print(Inspect(Targets))
+    print('^----------------------------------------------------------------')
 end
 
 
@@ -239,6 +330,10 @@ function GameScreen:load_level(filename)
 
     World = Bump.newWorld(32)
 
+    -- Reset Entities to keep from accum
+    Entities = nil
+    Entities = {}
+
     -- Prepare collision objects
     Map:bump_init(World)
 
@@ -246,28 +341,26 @@ function GameScreen:load_level(filename)
 
     local sprite_layer = Map.layers['Sprite Layer']
 
-    local object_group = Map.layers['objectgroup']
-
-    print('object_group')
-    print(Inspect(object_group))
-    print('----')
-    print('object_group.objects')
-    print(Inspect(object_group.objects))
-
     for k, object in pairs(Map.objects) do
-        print(Inspect(object))
         if object.name == 'Player' then
-            spawn_point = object
-            Player.x = spawn_point.x + 16
-            Player.y = spawn_point.y + 32
+            Player.x = object.x + 16
+            Player.y = object.y + 32
             World:add(Player, Player.x, Player.y, Player.width, Player.height)
-            break
+
+        elseif object.type == 'Box' then
+            -- Entities / Boxes
+            obj = Box:new()
+            obj.x = object.x
+            obj.y = object.y
+            Entities[k] = obj
+            World:add(Entities[k], obj.x, obj.y, obj.width, obj.height)
         end
     end
 
     -- Attach our player to the Map layer
     sprite_layer.sprites = {
-        player = Player
+        player = Player,
+        entities = Entities
     }
 
     -- Update callback for Custom Layer
@@ -282,27 +375,54 @@ function GameScreen:load_level(filename)
     -- Draw callback for Custom Layer
     function sprite_layer:draw()
         for _, sprite in pairs(self.sprites) do
-            Player.animation:draw(
-                sprite.spritesheet,
-                math.floor(sprite.x),
-                math.floor(sprite.y),
-                sprite.rotation,
-                1,
-                1,
-                sprite.offset_x,
-                sprite.offset_y
-            )
+            if sprite.type == 'player' then
+                Player.animation:draw(
+                    sprite.spritesheet,
+                    math.floor(sprite.x),
+                    math.floor(sprite.y),
+                    sprite.rotation,
+                    1,
+                    1,
+                    sprite.offset_x,
+                    sprite.offset_y
+                )
+            else
+                for _, entity_sprite in pairs(sprite) do
+                    entity_sprite.animation:draw(
+                        entity_sprite.spritesheet,
+                        math.floor(entity_sprite.x),
+                        math.floor(entity_sprite.y),
+                        entity_sprite.rotation,
+                        1,
+                        1,
+                        entity_sprite.offset_x,
+                        entity_sprite.offset_y
+                    )
+                end
+            end
 
             -- Temporarily draw a point at our location so we know
             -- that our sprite is offset properly
             if Debug then
-                love.graphics.rectangle(
-                    'fill',
-                    math.floor(sprite.x),
-                    math.floor(sprite.y),
-                    math.floor(sprite.width),
-                    math.floor(sprite.height)
-                )
+                if sprite.type == 'player' then
+                    love.graphics.rectangle(
+                        'fill',
+                        math.floor(sprite.x),
+                        math.floor(sprite.y),
+                        math.floor(sprite.width),
+                        math.floor(sprite.height)
+                    )
+                else
+                    for _, entity_sprite in pairs(sprite) do
+                    love.graphics.rectangle(
+                        'fill',
+                        math.floor(entity_sprite.x),
+                        math.floor(entity_sprite.y),
+                        math.floor(entity_sprite.width),
+                        math.floor(entity_sprite.height)
+                    )
+                    end
+                end
             end
         end
     end
@@ -329,21 +449,15 @@ function GameScreen:update(deltatime)
         Player.y = Player.y - Player.speed * deltatime
         Player.animation = Player.animations.up
         animate_player = true
-    end
-
-    if love.keyboard.isDown('down') or love.keyboard.isDown('s') then
+    elseif love.keyboard.isDown('down') or love.keyboard.isDown('s') then
         Player.y = Player.y + Player.speed * deltatime
         Player.animation = Player.animations.down
         animate_player = true
-    end
-
-    if love.keyboard.isDown('left') or love.keyboard.isDown('a') then
+    elseif love.keyboard.isDown('left') or love.keyboard.isDown('a') then
         Player.x = Player.x - Player.speed * deltatime
         Player.animation = Player.animations.left
         animate_player = true
-    end
-
-    if love.keyboard.isDown('right') or love.keyboard.isDown('d') then
+    elseif love.keyboard.isDown('right') or love.keyboard.isDown('d') then
         Player.x = Player.x + Player.speed * deltatime
         Player.animation = Player.animations.right
         animate_player = true
@@ -355,12 +469,41 @@ function GameScreen:update(deltatime)
 
     Player.x, Player.y, collisions, collision_len = World:move(Player, Player.x, Player.y)
     if collision_len > 0 then
-        if Debug then
-            print(collision_len)
-            print(Inspect(collisions))
-            for i=1,collision_len do
-                local collision = collisions[i]
-                print(collision.other)
+        -- print(Inspect(collisions))
+        for i=1,collision_len do
+            local collision = collisions[i]
+            -- print(collision.other.id)
+            -- print(collision.other.type)
+            -- print(collision.other.properties.movable)
+            -- print(collision.other.y)
+            if collision and collision.other.properties.movable then
+                destination_x = collision.other.x
+                destination_y = collision.other.y
+
+                object_group = Map.objects
+                for _, sprite in pairs(object_group) do
+                    -- print(sprite.x == collision.other.x and sprite.y == collision.other.y)
+                    -- if sprite.rotation then
+                    --     sprite.rotation = sprite.rotation + math.rad(90 * deltatime)
+                    -- end
+                end
+
+                -- print(Map:convertPixelToTile(collision.other.x, collision.other.y))
+
+                -- There are probably better ways to do this, but we push the block
+                -- based on the direction that our player is facing
+
+                if Player.animation == Player.animations.up then
+                    destination_y = destination_y - 64
+                elseif Player.animation == Player.animations.down then
+                    destination_y = destination_y + 64
+                elseif Player.animation == Player.animations.left then
+                    destination_x = destination_x - 64
+                elseif Player.animation == Player.animations.right then
+                    destination_x = destination_x + 64
+                end
+
+                collision.other.x, collision.other.y, collisions, collision_len = World:move(collision.other, destination_x, destination_y)
             end
         end
     end
